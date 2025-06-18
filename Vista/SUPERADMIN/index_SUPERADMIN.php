2a03458ccf858992a81993bfba259c496d646346
Moises <moises.gonzalez.dev@gmail.com>
2025-06-17 20:28:28 -0600

    Update index_SUPERADMIN.php

--- Vista/SUPERADMIN/index_SUPERADMIN.php
+++ Vista/SUPERADMIN/index_SUPERADMIN.php
@@ -9,7 +9,7 @@ if(isset($_GET['cerrar_sesion'])) { // Verificar si se solicita cerrar sesión
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title>SUPERADMIN</title>
+    <title>Usuario Programador</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <!-- Chart.js adapter for Luxon -->
